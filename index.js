e1d125d8cf9f2fc1e2131728228ac5a4a2501948
Flow-RandD <sam.walker@flowstore.co.uk>
2018-08-01 15:13:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+restService.post('/Find me a zinc bracket', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.product-material ? req.body.result.parameters.product-material : "Seems like there's a problem with the material. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
